b43297b07441cf22bc39d5c28b01561e24537acc
Marisha <marishaderoubaix@marishas-macbook-pro.local>
2018-09-12 13:45:31 +0100

    Getting Classifications from flowerClassifierCoreML

--- flowerRecognitionCoreML/ViewController.swift
+++ flowerRecognitionCoreML/ViewController.swift
@@ -7,14 +7,65 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+  @IBOutlet weak var imageViewOutlet: UIImageView!
+  @IBOutlet weak var label: UILabel!
+  
+  let wikipediaURl = "https://en.wikipedia.org/w/api.php"
+  let imagePicker = UIImagePickerController()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    imagePicker.delegate = self
+    
+  }
+  
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    if let userPickedImage = info[.editedImage] as? UIImage {
+      imageViewOutlet.image = userPickedImage
+      imagePicker.dismiss(animated: true, completion: nil)
+      
+      guard let ciImage = CIImage(image: userPickedImage) else {
+        fatalError("Could not convert UIImage to CIImage")
+      }
+      detect(image: ciImage)
+    }
+  }
+  
+  func detect(image: CIImage) {
+    
+    guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+      fatalError("Error loading coreML")
+    }
+    
+    let request = VNCoreMLRequest(model: model) { (request, error) in
+      guard let classification = request.results?.first as? VNClassificationObservation else {
+        fatalError("Model failed to process image")
+      }
+      self.navigationItem.title = classification.identifier.capitalized
+      
+    }
+    
+    let handler = VNImageRequestHandler(ciImage: image)
+    
+    do {
+      try handler.perform([request])
+    } catch {
+      print(error)
+    }
   }
 
 
+  @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
+    
+    imagePicker.allowsEditing = true
+    imagePicker.sourceType = .camera
+    present(imagePicker, animated: true, completion: nil)
+  }
 }
 
