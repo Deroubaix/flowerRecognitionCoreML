90f1ff57387fdf31a365b01f51fb83d1e9a1cb76
Marisha <marishaderoubaix@marishas-macbook-pro.local>
2018-09-12 14:01:56 +0100

    HTTP GET requests to Wikipedia using Alamofire and REST

--- flowerRecognitionCoreML/ViewController.swift
+++ flowerRecognitionCoreML/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 import CoreML
 import Vision
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -48,6 +50,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         fatalError("Model failed to process image")
       }
       self.navigationItem.title = classification.identifier.capitalized
+      self.requestInfo(flowerName: classification.identifier)
       
     }
     
@@ -57,6 +60,31 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
       try handler.perform([request])
     } catch {
       print(error)
+    }
+  }
+  
+  func requestInfo(flowerName: String) {
+    
+    let parameters : [String:String] = [
+      "format" : "json",
+      "action" : "query",
+      "prop" : "extracts",
+      "exintro" : "",
+      "explaintext" : "",
+      "titles" : flowerName,
+      "indexpageids" : "",
+      "redirects" : "1",
+      ]
+    
+    Alamofire.request(wikipediaURl, method: .get, parameters: parameters).responseJSON { (response) in
+      if response.result.isSuccess {
+        print(JSON(response.result.value!))
+        
+        let flowerJSON : JSON = JSON(response.result.value!)
+        let pageid = flowerJSON["query"]["pageids"][0].stringValue
+        let flowerDescription = flowerJSON["query"]["pages"][pageid]["extract"].stringValue
+        self.label.text = flowerDescription
+      }
     }
   }
 
